82454397fd0874f42aae32a6dfda1456cd517c13
keisuke ohashi <kog71903@gmail.com>
2019-09-11 10:40:20 +0900

    [add]Userにwordsとtitleを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_020121) do
+ActiveRecord::Schema.define(version: 2019_09_11_013453) do
 
   create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2019_09_10_020121) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "words"
+    t.string "title"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
