a1399108e17c572f9c4712aacfeebcb15f889cda
keisuke ohashi <kog71903@gmail.com>
2019-09-12 11:46:39 +0900

    [update]Userにnameを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_022103) do
+ActiveRecord::Schema.define(version: 2019_09_12_024025) do
 
   create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 2019_09_12_022103) do
     t.datetime "updated_at", null: false
     t.string "words"
     t.string "title"
+    t.string "name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
