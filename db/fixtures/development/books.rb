74ac11cfb9e335fe64459a80fe4dcd3b5534874d
keisuke ohashi <kog71903@gmail.com>
2019-09-14 18:01:03 +0900

    [add]seed-fuを追加 & seedのファイルを作成

---
+++ db/fixtures/development/books.rb
@@ -0,0 +1,6 @@
+Book.seed do |s|
+  s.id = 1
+  s.title = "Is It Wrong to Try to Pick Up Girls in a Dungeon?, Vol. 13 (light novel) (Is It Wrong to Pick Up Girls in a Dungeon?)"
+  s.isbn = "1975328191"
+  s.pages = 224
+end
