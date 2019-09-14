5f6e97da24c2cf421a55bc382ebadf26085994bf
keisuke ohashi <kog71903@gmail.com>
2019-09-14 20:31:33 +0900

    [add]基本的なvalidationを各modelに設定

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,6 @@
 class Author < ApplicationRecord
   has_many :read_books, as: :readable
   has_many :books, as: :readable
+
+  validates :name, presence: true
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -5,6 +5,12 @@ class Book < ApplicationRecord
   has_many :users, through: :reviews
   belongs_to :author
 
+  validates :title, presence: true
+  validates :isbn, presence: true, uniqueness: true
+  validates :pages, presence: true
+  validates :url, presence: true, uniqueness: true
+  validates :image_url, presence: true
+
   def self.find_books(keyword)
     SearchAmazon.search_books(keyword)
   end
