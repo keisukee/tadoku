fdd43dceb870b1cf5bf96ab5bb85a13e947cf540
keisuke ohashi <kog71903@gmail.com>
2019-09-16 00:09:08 +0900

    [update]グラフに表示する横軸と縦軸のデータをgonでcontrollerにて設定 & viewに反映

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    gon.graph_data_array = @user.calc_words_per_month
+    gon.months = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"]
+    gon.book_counts = []
+    gon.cumulated_book_number = @user.cumulated_book_number
+    gon.calc_cumulative_words = @user.calc_cumulative_words
   end
 
   def update
