cec95fe221b4d0f8267facaae92cd5a2535c1959
keisuke ohashi <kog71903@gmail.com>
2019-09-18 01:09:00 +0900

    [update]ReadとReadBookにasinを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_15_014522) do
+ActiveRecord::Schema.define(version: 2019_09_17_155123) do
 
   create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2019_09_15_014522) do
     t.string "image_url"
     t.integer "image_height"
     t.integer "image_width"
+    t.string "asin"
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
@@ -62,6 +63,7 @@ ActiveRecord::Schema.define(version: 2019_09_15_014522) do
     t.string "image_url"
     t.integer "image_height"
     t.integer "image_width"
+    t.string "asin"
     t.index ["readable_type", "readable_id"], name: "index_read_books_on_readable_type_and_readable_id"
   end
 
