ca34ed70da02b8709073fccbb67b4fe599875505
keisuke ohashi <kog71903@gmail.com>
2019-09-20 21:15:56 +0900

    [update]web_consoleをdocker上でも開けるように

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.web_console.whitelisted_ips = %w[0.0.0.0/0 ::/0]
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
 end
