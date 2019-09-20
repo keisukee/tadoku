6d39ef6e1654f738214ef4c87d344f67a90974c3
keisuke ohashi <kog71903@gmail.com>
2019-09-21 00:02:53 +0900

    [update]Userのreview一覧ページ作成

--- app/controllers/users/reviews_controller.rb
+++ app/controllers/users/reviews_controller.rb
@@ -1,4 +1,6 @@
 class Users::ReviewsController < ApplicationController
   def index
+    @user = User.find(params[:user_id])
+    @reviews = @user.reading_histories.where(user_id: @user.id).where.not(review: nil)
   end
 end
