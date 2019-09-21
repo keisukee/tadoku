abc08e6aff9676fb4cbb25551a4dfff2f5abd797
keisuke ohashi <kog71903@gmail.com>
2019-09-21 13:44:50 +0900

    [update]letter_openerからletter_opener_webに変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,6 +63,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
 
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.perform_deliveries = true
 end
