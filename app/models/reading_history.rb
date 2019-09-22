281ac7791e7b864883dce61abd62f790ee931bd4
keisuke ohashi <kog71903@gmail.com>
2019-09-22 13:16:13 +0900

    [update]statusのenumに

--- app/models/reading_history.rb
+++ app/models/reading_history.rb
@@ -1,5 +1,6 @@
 class ReadingHistory < ApplicationRecord
   enum genre: {adventure: 0, sf: 1, art: 2, biography: 3, classics: 4, comedy: 5, crime_suspense: 6, easy_story: 7, history: 8, heart_warming: 9, philosophy: 10, politics: 11, rhyme: 12, sport: 13, short_stories: 14, true_story: 15, manga: 16, love: 17, other: 18}
+  enum status: {wish: 0, stacked: 1, reading: 2, read: 3}
 
   belongs_to :user
   belongs_to :book
