7747962bd4ecf0bce5f6f3c2570f997735a362f8
keisuke ohashi <kog71903@gmail.com>
2019-09-25 14:10:12 +0900

    [update]アカウント情報の変更ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,12 +14,39 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find_by(id: params[:id])
+    unless user_params["start_at(1i)"].nil?
+      year = user_params["start_at(1i)"].to_i
+      month = user_params["start_at(2i)"].to_i
+      day = user_params["start_at(3i)"].to_i
+      hour = user_params["start_at(4i)"].to_i
+      minute = user_params["start_at(5i)"].to_i
+    end
+    @start_at = DateTime.new(year, month, day, hour, minute) || DateTime.now
+
+    if @user.update(name: user_params[:name],
+                    image: user_params[:image],
+                    site_name: user_params[:site_name],
+                    site_url: user_params[:site_url],
+                    introduction: user_params[:introduction],
+                    start_at: @start_at)
+      flash[:success] = "アカウント情報の更新が完了しました"
+    else
+      flash[:danger] = "アカウント情報の更新が失敗しました"
+    end
+    redirect_to account_users_path
   end
 
   def destroy
   end
 
   def account
+    @user = current_user
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :image, :site_name, :site_url, :introduction, "start_at(1i)", "start_at(2i)", "start_at(3i)", "start_at(4i)", "start_at(5i)")
   end
 end
